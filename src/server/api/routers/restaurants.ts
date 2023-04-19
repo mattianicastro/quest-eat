a4a7456504aef1c195698c964530d9c758cd2750
Mattia Nicastro <nicastromattia@outlook.com>
2023-04-19 08:08:45 +0200

    first commit

---
+++ src/server/api/routers/restaurants.ts
@@ -0,0 +1,69 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+
+export const restaurantsRouter = createTRPCRouter({
+  createRestaurant: protectedProcedure
+    .input(z.object({
+        name: z.string().max(100),
+        address: z.string().max(500),
+        city: z.string().max(100),
+        state: z.string().max(100),
+        zip: z.number(),
+        lat: z.number().min(-90).max(90),
+        lng: z.number().min(-180).max(180),
+        phone: z.string().max(100).optional(),
+        website: z.string().url().optional(),
+        email: z.string().email().optional(),
+    }))
+    .mutation(async ({ input, ctx }) => {
+      await ctx.prisma.restaurant.create(
+        {
+            data: {
+                name: input.name,
+                address: input.address,
+                city: input.city,
+                state: input.state,
+                zip: input.zip,
+                lat: input.lat,
+                lng: input.lng,
+                phone: input.phone,
+                website: input.website,
+                email: input.email,
+                createdBy: {
+                    connect: {
+                        id: ctx.session.user.id,
+                    },
+                }
+            }
+        }
+      )
+      return {
+        ok: true,
+      };
+    }),
+  delete: protectedProcedure.input(z.object({
+    id: z.string(),
+})).mutation(async ({ input, ctx }) => {
+    await ctx.prisma.restaurant.delete({
+        where: {
+            id: input.id,
+        },
+    });
+    return {
+        ok: true,
+    };
+}),
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.restaurant.findMany();
+  }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
