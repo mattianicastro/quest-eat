c9af6b44be46ece253ad0ed81d76368ff7573674
Mattia Nicastro <nicastromattia@outlook.com>
2023-04-26 02:15:58 +0200

    add addrestaurant

--- src/pages/restaurants.tsx
+++ src/pages/restaurants.tsx
@@ -1,19 +1,34 @@
 import type { NextPage } from "next";
 import { api } from "~/utils/api";
 import { Loading } from "~/components/Loading";
+import { useRouter } from "next/router";
+
 import RestaurantCard from "~/components/RestaurantCard";
+import { AddRestaurantModal } from "~/components/AddRestaurantModal";
+import type { PartialRestaurant } from "~/components/AddRestaurantModal";
+
 
 export const Restaurants: NextPage = () => {
     const restaurantsQuery = api.restaurants.getAll.useQuery();
+    const addRestaurantMutation = api.restaurants.createRestaurant.useMutation();
+    const router = useRouter();
 
     if (restaurantsQuery.isLoading) {
         return <Loading />;
+    }
+    
+    function handleAddRestaurant(restaurant: PartialRestaurant) {
+        void addRestaurantMutation.mutateAsync(restaurant).then((res)=>{
+            void router.push(`/restaurants/${res.id}`)
+        });
+
     }
 
     return (
         <div className="flex flex-col p-4">
             <div className="mb-5 flex items-center justify-between">
                 <h1 className="text-3xl font-bold">Ristoranti</h1>
+            <AddRestaurantModal onAdd={handleAddRestaurant}/>
             </div>
             <div className="flex flex-row flex-wrap justify-center gap-6">
                 {restaurantsQuery.data ? (
