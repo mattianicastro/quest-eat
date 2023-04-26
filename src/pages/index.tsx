f80151b19953df0bb980d933b0bc4d1646cedce6
Mattia Nicastro <nicastromattia@outlook.com>
2023-04-26 09:39:17 +0200

    change logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const Index: NextPage = () => {
     return (
         <div className="min-h-[calc(100vh-3.5rem)] h-max flex flex-col justify-center items-center p-5 gap-y-4">
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-10 text-red-500">Quest eat</h1>
-            <Image src={"/content-crop-logo.png"} alt={"Logo"} width={400} height={400}></Image>
+            <Image src={"/quest_eat_circle.png"} alt={"Logo"} width={400} height={400}></Image>
             <Button className="bg-gradient-to-r bg-red-600 text-white" href={"/tours"}>Esplora</Button>
         </div>
 
